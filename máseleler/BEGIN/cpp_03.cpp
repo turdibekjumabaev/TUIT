63499d985877dedcc87fad6f21577287c8a01df1
Turdibek <110396648+turdibekjumabaev@users.noreply.github.com>
2023-01-10 14:39:11 +0500

    Begin 3

---
+++ máseleler/BEGIN/cpp_03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout << "a= ";
+  cin >> a;
+  cout << "b= ";
+  cin >> b;
+  cout << "S= " << a*b << "\nP= " <<  2*(a+b);
+  return 0;
+}
